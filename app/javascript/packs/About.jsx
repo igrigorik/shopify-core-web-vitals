a4a38fc96dcb121f324e25d8a4dad8cd6cf1acb4
Ilya Grigorik <ilya@igvita.com>
2020-07-26 00:11:38 -0700

    a more coherent header intro & description

--- app/javascript/packs/About.jsx
+++ app/javascript/packs/About.jsx
@@ -4,49 +4,33 @@ import { MediaCard, Link, TextContainer } from "@shopify/polaris";
 function About(props) {
   return (
     <MediaCard
-      title="Core Web Vitals"
+      title="Essential user experience metrics for a healthy store"
       primaryAction={{
-        content: "Customize benchmark sites",
+        content: "Customize benchmark stores",
         onAction: () => {},
       }}
       secondaryAction={{
-        content: "Learn about Core Web Vitals on web.dev",
+        content: "Learn more about Web Vitals",
         external: true,
-        url: "https://web.dev/vitals/#core-web-vitals",
+        url: "https://web.dev/vitals/",
       }}
       description={
         <>
-          Delivering fast loading, responsive, and visually stable user
-          experience is critical to every shop's long-term success on the web.
-          Core Web Vitals are a set of metrics and thresholds—defined by
-          Google's{" "}
+          Optimizing quality of user experience is critical to the success of
+          every storefront.{" "}
           <Link
             url="https://blog.chromium.org/2020/05/introducing-web-vitals-essential-metrics.html"
             external={true}
             monochrome={true}
           >
-            Web Vitals program
-          </Link>
-          — that capture these core user experience needs.
+            Core Web Vitals
+          </Link>{" "}
+          are a set of metrics and thresholds that capture the core user
+          experience needs of fast loading experience, interactivity, and visual
+          stability.
           <br />
           <br />
-          Core Web Vitals are{" "}
-          <Link
-            url="https://developers.google.com/web/fundamentals/performance/speed-tools"
-            external={true}
-            monochrome={true}
-          >
-            field measurable
-          </Link>
-          , have supporting lab diagnostic metric equivalents and{" "}
-          <Link
-            url="https://web.dev/vitals-tools/"
-            external={true}
-            monochrome={true}
-          >
-            tooling
-          </Link>
-          , and part of the{" "}
+          Core Web Vitals are part of the upcoming{" "}
           <Link
             url="https://webmasters.googleblog.com/2020/05/evaluating-page-experience.html"
             external={true}
@@ -54,10 +38,31 @@ function About(props) {
           >
             page experience signal
           </Link>{" "}
-          for Google Search.
+          for Google Search, are widely available across popular Google web
+          developer tools, and can be tracked by every site owner both in{" "}
+          <Link
+            url="https://web.dev/vitals-tools/"
+            external={true}
+            monochrome={true}
+          >
+            development and in production
+          </Link>
+          .
+          <br />
+          <br />
+          This report provides a view into how real-world Google Chrome users
+          experience your Shopify-powered storefront, as captured by the{" "}
+          <Link
+            url="https://developers.google.com/web/tools/chrome-user-experience-report"
+            external={true}
+            monochrome={true}
+          >
+            Chrome UX Report
+          </Link>
+          , and enables you to benchmark your site against a custom list of
+          competitors.
         </>
       }
-      // popoverActions={[{ content: "Dismiss", onAction: () => {} }]} // TODO
       size="small"
     >
       <img
