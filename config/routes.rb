5f4bcc9f24766ed29bf4717b6a76d3c022599767
Ilya Grigorik <ilya@igvita.com>
2020-08-02 14:47:11 -0700

    add privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root :to => 'home#index'
-  mount ShopifyApp::Engine, at: '/'
+  root to: "home#index"
+  mount ShopifyApp::Engine, at: "/"
 
-  get    '/competitors', to: 'competitors#show'
-  post   '/competitors', to: 'competitors#create'
-  delete '/competitors', to: 'competitors#destroy'
+  get "/privacy", to: "home#privacy"
+  get "/competitors", to: "competitors#show"
+  post "/competitors", to: "competitors#create"
+  delete "/competitors", to: "competitors#destroy"
 end
